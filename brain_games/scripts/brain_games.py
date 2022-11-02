ff60fd3625dab5d46d59347622dda549905e6d8e
Олег Потуданский <olegovych@bk.ru>
2022-11-03 00:06:09 +0300

    update brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+import brain_games.cli
+
 def welcome():
     print('Welcome to the Brain Games!')
 
 def main():
     welcome()
+    brain_games.cli.welcome_user()
 
 if __name__ == '__main__':
     main()
