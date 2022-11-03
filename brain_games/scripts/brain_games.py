7febcbc790b56f8f8eee58503ec8e86ce350206b
Олег Потуданский <olegovych@bk.ru>
2022-11-03 14:10:20 +0300

    fix brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 import brain_games.cli
 
+
 def welcome():
     print('Welcome to the Brain Games!')
+
 
 def main():
     welcome()
     brain_games.cli.welcome_user()
 
+
 if __name__ == '__main__':
     main()
-
