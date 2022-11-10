753726b3c4166e64577d4b778169f41486d7c73e
Олег Потуданский <olegovych@bk.ru>
2022-11-10 13:13:44 +0300

    add brain_calc.py

---
+++ brain_games/scripts/games/brain_calc.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from brain_games.functionz \
+    import welcome_user, check_answer
+
+
+def main():
+    user_name = welcome_user()
+    print('What is the result of the expression?')
+    func_index = 1
+    check_answer(user_name, func_index)
+
+
+if __name__ == '__main__':
+    main()
