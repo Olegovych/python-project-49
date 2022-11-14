733f0a950cb385cff6b3ba10e6ea246b67da8261
Олег Потуданский <olegovych@bk.ru>
2022-11-14 11:42:57 +0300

    update functionz.py

--- brain_games/functionz.py
+++ brain_games/functionz.py
@@ -44,8 +44,21 @@ def calc():
     return question, correct_answer
 
 
+def progression():
+    pr_start = randint(1, 20)
+    pr_step = randint(1, 10)
+    pr_len = randint(5, 10)
+    pr_list = list(str(_) for _ in range
+        (pr_start, pr_start + pr_len * pr_step, pr_step))
+    question_index = randint(0, pr_len - 1)
+    correct_answer = pr_list[question_index]
+    pr_list[question_index] = '..'
+    question = ' '.join(pr_list)
+    return question, correct_answer
+
+
 def check_answer(user_name, func_index):
-    functions = (even, calc, gcd)
+    functions = (even, calc, gcd, progression)
     for _ in range(3):
         question, correct_answer = functions[func_index]()
         print(f'Question: {question}')
