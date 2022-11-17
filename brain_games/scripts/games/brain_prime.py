cac18242fe35a9525a660fe272f1a06efae3c7c1
Олег Потуданский <olegovych@bk.ru>
2022-11-17 21:01:37 +0300

    update brain_prime.py

--- brain_games/scripts/games/brain_prime.py
+++ brain_games/scripts/games/brain_prime.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.functionz import welcome_user, check_answer
+from brain_games.functionz import check_answer, prime
 
 
 def main():
-    user_name = welcome_user()
-    print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    func_index = 4
-    check_answer(user_name, func_index)
+    task = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    check_answer(task, prime)
 
 
 if __name__ == '__main__':
