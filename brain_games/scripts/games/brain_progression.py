f00360fabc2d0ef2a40c6a7e8f0013a3cf8e2406
Олег Потуданский <olegovych@bk.ru>
2022-11-17 21:02:32 +0300

    update brain_progression.py

--- brain_games/scripts/games/brain_progression.py
+++ brain_games/scripts/games/brain_progression.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.functionz import welcome_user, check_answer
+from brain_games.functionz import check_answer, progression
 
 
 def main():
-    user_name = welcome_user()
-    print('What number is missing in the progression?')
-    func_index = 3
-    check_answer(user_name, func_index)
+    task = 'What number is missing in the progression?'
+    check_answer(task, progression)
 
 
 if __name__ == '__main__':
