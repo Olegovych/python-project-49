6c12a882e6c00c183eeba829546370847bc2e257
Олег Потуданский <olegovych@bk.ru>
2022-11-22 21:47:52 +0300

    add game_prime.py

---
+++ brain_games/games/game_prime.py
@@ -0,0 +1,14 @@
+from random import randint
+
+
+task = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+PRIME_LIST = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+              41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+
+def prime():
+    question = randint(0, 100)
+    if question in PRIME_LIST:
+        return question, 'yes'
+    else:
+        return question, 'no'
