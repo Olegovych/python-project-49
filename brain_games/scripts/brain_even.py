1ecff3fb76705db2fa51237d4672d696877bcabe
Олег Потуданский <olegovych@bk.ru>
2022-11-22 21:45:26 +0300

    update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from brain_games.functionz import check_answer, even
+from brain_games.functionz import check_answer
+from brain_games.games.game_even import task, even
 
 
 def main():
-    task = 'Answer "yes" if the number is even, otherwise answer "no".'
     check_answer(task, even)
 
 
