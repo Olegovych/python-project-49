992e91278cf76c663025012300790312bb16fda1
Олег Потуданский <olegovych@bk.ru>
2022-11-22 21:40:48 +0300

    add game_calc.py

---
+++ brain_games/games/game_calc.py
@@ -0,0 +1,13 @@
+from random import randint, choice
+
+
+task = 'What is the result of the expression?'
+
+
+def calc():
+    first_num = randint(0, 100)
+    operator = choice('+-*')
+    second_num = randint(0, 10)
+    question = f'{first_num} {operator} {second_num}'
+    correct_answer = str(eval(question))
+    return question, correct_answer
