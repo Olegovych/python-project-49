b4cf543ccb499e5584ab62a073add343ec4d52d4
Олег Потуданский <olegovych@bk.ru>
2022-11-22 21:46:45 +0300

    add game_gcd.py

---
+++ brain_games/games/game_gcd.py
@@ -0,0 +1,16 @@
+from random import randint
+
+
+task = 'Find the greatest common divisor of given numbers.'
+
+
+def gcd():
+    first_num = randint(1, 100)
+    second_num = randint(1, 100)
+    question = f'{first_num} {second_num}'
+    smaller, bigger = sorted((first_num, second_num))
+    if bigger % smaller == 0:
+        return question, str(smaller)
+    for div in range(smaller // 2, 0, -1):
+        if bigger % div == 0 and smaller % div == 0:
+            return question, str(div)
