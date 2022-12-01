51ec5916c8018da08834c6940df161f8b09d3801
Олег Потуданский <olegovych@bk.ru>
2022-12-01 20:48:17 +0300

    update calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,4 +1,5 @@
 from random import randint, choice
+import operator
 
 
 TASK = 'What is the result of the expression?'
@@ -6,8 +7,13 @@ TASK = 'What is the result of the expression?'
 
 def get_question_and_answer():
     first_num = randint(0, 100)
-    operator = choice('+-*')
     second_num = randint(0, 10)
-    question = f'{first_num} {operator} {second_num}'
-    correct_answer = str(eval(question))
+    operations = (
+        ('+', operator.add),
+        ('-', operator.sub),
+        ('*', operator.mul)
+    )
+    operation_name, operation_method = choice(operations)
+    question = f'{first_num} {operation_name} {second_num}'
+    correct_answer = str(operation_method(first_num, second_num))
     return question, correct_answer
