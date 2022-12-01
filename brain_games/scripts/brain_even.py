da11ccded6ff875a2561f034c39aff4c6777d059
Олег Потуданский <olegovych@bk.ru>
2022-12-01 18:42:28 +0300

    rename function even to get_question_and_answer

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from brain_games.engine import run_game
-from brain_games.games.even import TASK, even
+from brain_games.games.even import TASK, get_question_and_answer
 
 
 def main():
-    run_game(TASK, even)
+    run_game(TASK, get_question_and_answer)
 
 
 if __name__ == '__main__':
