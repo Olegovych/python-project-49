c863eeaef48fdbf9f0436cd83905e0ec7d1c9f74
Олег Потуданский <olegovych@bk.ru>
2022-12-01 18:00:58 +0300

    rename game_progression.py to progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from brain_games.engine import run_game
-from brain_games.games.game_progression import TASK, progression
+from brain_games.games.progression import TASK, progression
 
 
 def main():
