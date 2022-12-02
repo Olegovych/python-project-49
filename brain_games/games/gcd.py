50d980812b35d977eb819094b23aa6586c7d4ee2
Олег Потуданский <olegovych@bk.ru>
2022-12-02 10:23:25 +0300

    update gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,5 +1,5 @@
 from random import randint
-
+from math import gcd
 
 TASK = 'Find the greatest common divisor of given numbers.'
 
@@ -8,9 +8,4 @@ def get_question_and_answer():
     first_num = randint(1, 100)
     second_num = randint(1, 100)
     question = f'{first_num} {second_num}'
-    smaller, bigger = sorted((first_num, second_num))
-    if bigger % smaller == 0:
-        return question, str(smaller)
-    for div in range(smaller // 2, 0, -1):
-        if bigger % div == 0 and smaller % div == 0:
-            return question, str(div)
+    return question, str(gcd(first_num, second_num))
