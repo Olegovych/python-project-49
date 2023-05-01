e14aa5dac1f6a870fd876baed75841955d3ae918
Олег Потуданский <olegovych@bk.ru>
2023-05-01 19:07:52 +0300

    refactor prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,18 +1,16 @@
 from random import randint
 
-
 TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def get_question_and_answer():
-    question = randint(2, 100)
-    if is_prime(question):
-        return question, 'yes'
-    else:
-        return question, 'no'
+    question = randint(-100, 100)
+    return (question, 'yes') if is_prime(question) else (question, 'no')
 
 
 def is_prime(number):
+    if number <= 1:
+        return False
     for divider in range(2, number // 2 + 1):
         if number % divider == 0:
             return False
