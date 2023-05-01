77c4aaf6b1eaa937da31ec5cda367fb2d83b4676
Олег Потуданский <olegovych@bk.ru>
2023-05-01 19:07:11 +0300

    refactor even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,12 +1,8 @@
 from random import randint
 
-
 TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def get_question_and_answer():
     question = randint(0, 100)
-    if question % 2 == 0:
-        return question, 'yes'
-    else:
-        return question, 'no'
+    return (question, 'yes') if question % 2 == 0 else (question, 'no')
